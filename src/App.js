65393df25897244253405fa14b90c5faa44735ab
MaciejP <m.ponicki1995@gmail.com>
2024-09-13 20:54:39 +0200

    check if everything works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  let [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://cat-fact.herokuapp.com/facts").then((res) => {
+      const { data } = res;
+      setFetchedData(data);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">Initialization...</header>
+      <header className="App-header">
+        {fetchedData.map((item, index) => (
+          <div key={index}>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </header>
     </div>
   );
 }
